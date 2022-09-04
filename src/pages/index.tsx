a8b607aee092d5bbe807bade4a3b9e6fcf838b21
smartinsdev <sinval.msjunior@gmail.com>
2022-09-04 11:41:01 -0300

    fix Invalid property 'fill' found on tag 'path' in svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import type { GetStaticProps, NextPage, InferGetStaticPropsType } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import Image from "next/image";
 
 import { Divider } from "../components/Divider";
 import { getAdvice, Data } from "../services/getAdvice";
+
+import iconDice from "../../public/images/icon-dice.svg";
 
 const Home: NextPage = ({
   props,
@@ -34,12 +37,9 @@ const Home: NextPage = ({
       </h1>
       <Divider />
       <button name="advice" className="btn" onClick={handleClick}>
-        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
-            fill="#202733"
-          />
-        </svg>
+        <div className="relative w-6 h-6">
+          <Image src={iconDice} alt="Genereitor Advice button" layout="fill" />
+        </div>
       </button>
     </>
   );
